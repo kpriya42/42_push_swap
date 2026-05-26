a523e4bea917be19ff0ccef45df4fca5d916f70b
Mengzhen Qian <meqian@1-B-29.42heilbronn.de>
2026-05-26 19:02:40 +0200

    adding the dealing arg function names and try to test parse arg function

--- include/push_swap.h
+++ include/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kri- <kri-@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: meqian <meqian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 17:26:07 by meqian            #+#    #+#             */
-/*   Updated: 2026/05/26 17:20:50 by kri-             ###   ########.fr       */
+/*   Updated: 2026/05/26 18:46:53 by meqian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@
 
 typedef struct s_stack
 {
-	int		value;
-	t_stack	*prev;
-	t_stack	*next;
+	int				value;
+	struct t_stack	*prev;
+	struct t_stack	*next;
 }	t_stack;
 
 typedef struct s_input
@@ -48,5 +48,16 @@ void	rra(t_stack **a);
 void	rrb(t_stack **b);
 void	rrr(t_stack **a, t_stack **b);
 
+int		check_dup(int *arry, int size);
+int		all_digit(char *av);
+void	free_split(char **splited_arry);
+void	error_exit(char **splited_arry);
+int		check_convert(char *av, char **splited_arry);
+
+char	**split_input(int ac, char **av);
+void	remove_flag(char **av, int index);
+int		strategy_selector(char **av);
+
+t_input	parse_args(int ac, char **av);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,18 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kri- <kri-@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: meqian <meqian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 09:53:08 by meqian            #+#    #+#             */
-/*   Updated: 2026/05/26 17:25:51 by kri-             ###   ########.fr       */
+/*   Updated: 2026/05/26 18:39:21 by meqian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-t_input	*parse_args(int ac, char **av)
+t_input	parse_args(int ac, char **av)
 {
-	
+	t_input	data;
+	char	**splited_av;
+	int		i;
+
+	i = 0;
+	splited_av = split_input(ac, av);
+	if (!splited_av)
+		exit(1);
+	data.strategy = strategy_selector(av);
+	data.size = 0;
+	while (splited_av[data.size])
+		data.size++;
+	data.numbers = malloc(sizeof(int) * data.size);
+	while (i < data.size)
+	{
+		data.numbers[i] = check_convert(splited_av[i], splited_av);
+		i ++;
+	}
+	if (check_dup(data.numbers, data.size))
+	{
+		free(data.numbers);
+		error_exit(splited_av);
+	}
+	free_split(splited_av);
+	return (data);
 }
 
 int	main(int ac, char **av)
@@ -24,6 +48,14 @@ int	main(int ac, char **av)
 	if (ac == 1)
 		return (0);
 	clean_data = parse_args(ac, av);
-
+	printf("strategy type : %d\n", clean_data.strategy);
+	printf("size of input : %d\n", clean_data.size);
+	int i = 0;
+	while (i < clean_data.size)
+	{
+		printf("args %d\n", clean_data.numbers[i]);
+		i ++;
+	}
+	printf("strategy type : %d", clean_data.strategy);
 	return (0);
 }
