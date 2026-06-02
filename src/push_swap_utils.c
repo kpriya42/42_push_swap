e6644c787e0e24b3b35c2c3cdd8d6fc47e252dda
Krishnapriya - <kri-@1-A-26.42heilbronn.de>
2026-06-02 12:22:59 +0200

    Added compute_disorder function

--- src/push_swap_utils.c
+++ src/push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: kri- <kri-@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 18:08:19 by kri-              #+#    #+#             */
-/*   Updated: 2026/05/28 18:23:17 by kri-             ###   ########.fr       */
+/*   Updated: 2026/06/02 12:21:31 by kri-             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,31 @@ t_stack	*find_last(t_stack *stack)
 		templist = templist->next;
 	}
 	return (templist);
+}
+
+double	compute_disorder(t_stack *head)
+{
+	t_stack	*current;
+	t_stack	*next;
+	long	mistakes;
+	long	total_pairs;
+
+	mistakes = 0;
+	total_pairs = 0;
+	current = head;
+	while (current)
+	{
+		next = current->next;
+		while (next)
+		{
+			total_pairs += 1;
+			if (current->value > next->value)
+				mistakes += 1;
+			next = next->next;
+		}
+		current = current->next;
+	}
+	if (total_pairs == 0)
+		return (0.00);
+	return ((double)mistakes / (double)total_pairs);
 }
